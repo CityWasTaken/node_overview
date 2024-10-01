38d7a2221b021573038f2fe44a83c2f4948ab338
CityWasTaken <smithfaheed@gmail.com>
2024-10-01 02:30:52 -0400

    started understanding of arrow functions

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
+const mathTools = {
+    num1: 1,
+    num2: 3,
+    sum: () => {
+    // sum: function(num1, num2) {
+    // should return the sum of num1 and num2
+    return this.num1 + this.num2;
+    },
+    difference: function() {
+    // should return the difference of num1 and num2
+    return this.num1 - this.num2;
+    }
+};
+
+const resultSum = mathTools.sum();
+const resultDifference = mathTools.difference();
+
+console.log(resultSum);
+console.log(resultDifference);
 
 
-function sum(a, b) {
-    console.log(a + b);
-}
+const date = new Date();
 
-sum(num1, num2);
 
+
+
+
+
+
+// function printName(userName) {
+//     const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+//     const capName = capitalize(userName);
+
+//     console.log(capName);
+    
+// }
+
+// printName('bob');
