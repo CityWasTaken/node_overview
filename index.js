d7e6c8c69c165a65ec604af5059665853ebd6709
CityWasTaken <smithfaheed@gmail.com>
2024-10-02 11:56:46 -0400

    working on wheel app welcome and menu system

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { log } = require('console');
 const fs = require('fs');
 const { clearInterval } = require('timers');
+const command = process.argv[2];
+const studentName = process.argv[3]
+
 
 function spinWheel() {
     fs.readFile('./names.txt', 'utf8', (error, data) => {
@@ -31,4 +34,46 @@ function spinWheel() {
     });
 }
 
-spinWheel();
+
+function addName() {
+    fs.writeFile('./names.txt', studentName, (error) => {
+        if (error) {
+            return console.log(error);
+        }
+
+
+        console.log('Name added successfully!');
+    });
+}
+
+function welcomeMessage() {
+    console.log(`
+        --------
+        Welsome to the Randome Name Wheel!
+        
+        To use this app, type in one of the options:
+        -spin -- This will spin the wheel
+        -add -- This adds a name to the list
+        
+        -------
+        `);
+    
+}
+
+
+switch(command) {
+    case 'add':
+        addName();
+        break;
+    case 'spin':
+        spinWheel();
+} 
+
+
+// if (command === 'add') {
+//     addName();
+// } else if (command === 'spin'){
+//     spinWheel();
+// }
+
+// spinWheel();
